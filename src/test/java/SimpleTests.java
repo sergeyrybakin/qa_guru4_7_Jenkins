18eb0a7400cd4b7de903bbdef9f134d440d3c6a5
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-17 19:59:36 +0700

    Add SimpleTests

---
+++ src/test/java/SimpleTests.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTests {
+    @Test
+    void successTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void negativeTest() {
+        assertTrue(false);
+    }
+
+}
